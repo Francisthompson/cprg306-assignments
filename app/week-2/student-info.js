29dbc43d7fe3be508c0cfca6957eadb6a44e644d
Francisthompson <147000819+Francisthompson@users.noreply.github.com>
2024-09-15 15:41:13 -0600

    Added Week 2 page and root page updates

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Francis Thompson</h1>
+      <p>
+        <Link href="https://github.com/Francisthompson" target="_blank">
+        https://github.com/Francisthompson
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
