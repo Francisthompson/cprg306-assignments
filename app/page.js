67e8c6b5a760f2922a05e8685a39f5f646e99956
Francisthompson <147000819+Francisthompson@users.noreply.github.com>
2024-09-20 16:55:18 -0600

    Added week 3 page, updated root page and other layout/tailwind elements for styling changes.

--- app/page.js
+++ app/page.js
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
+    <main className="bg-gray-900 text-white min-h-screen p-4">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <Link href="/week-2">Week 2 Assignment</Link>
-      </p>
+      <div>
+        <p>
+          <Link href="/week-2">Week 2 Assignment</Link>
+        </p>
+        <p>
+          <Link href="/week-3">Week 3 Assignment</Link>
+        </p>
+      </div>
     </main>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
+    <main className="bg-gray-900 text-white min-h-screen p-4">
       <h1>Shopping List</h1>
       <StudentInfo />
     </main>
